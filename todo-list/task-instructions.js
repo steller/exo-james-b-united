21fcbf0b2456feabefe25858265f16aa08314a57
Vincent Rouffiat <vrouffiat@norsys.fr>
2025-04-03 17:16:19 +0200

    feat(exo-js): add memory task before save it in bdd

--- todo-list/task-instructions.js
+++ todo-list/task-instructions.js
@@ -11,7 +11,7 @@ const INSTRUCTIONS = [
   },
   {
     id: 2,
-    title: "Changer le status d'une tâche",
+    title: "Changer le statut d'une tâche",
     callback: async () => {
       await changeStateTask();
     }
@@ -40,12 +40,17 @@ const INSTRUCTIONS = [
 ];
 module.exports.INSTRUCTIONS = INSTRUCTIONS;
 
+const tasksNotSaved = [];
+
 async function printListTasks() {
   const client = await getClient();
   const res = await client.query('SELECT * FROM taches WHERE statut <> $1 ORDER BY statut, id', ['terminée']);
-  if (res.rowCount > 0) {
+  if (res.rowCount > 0 || tasksNotSaved.length > 0) {
     console.log('Voici les tâches :');
-    res.rows.forEach(row => {
+    [
+      ...res.rows,
+      ...tasksNotSaved.filter(({ statut }) => statut !== 'terminée')
+    ].forEach(row => {
       const description = row.description ? ` (${row.description})` : '';
       console.log(`${row.id} - [${row.statut}] ${row.titre}${description}`);
     });
@@ -59,8 +64,18 @@ async function addTask() {
     question('Entrez le titre de la tâche :', (title) => {
       question('Entrez une description pour la tâche (optionnelle) :', async (description) => {
         const client = await getClient();
-        await client.query('INSERT INTO taches (titre, description) VALUES ($1, $2)', [title, description]);
-        console.log(`Tâche "${title}" ajoutée avec succès.`);
+        const res = await client.query('SELECT MAX(id) as maxid FROM taches');
+        const id = res.rows[0].maxid + tasksNotSaved.length + 1;
+        tasksNotSaved.push({
+          id,
+          titre: title,
+          description,
+          statut: 'à faire'
+        });
+
+        // const client = await getClient();
+        // await client.query('INSERT INTO taches (titre, description) VALUES ($1, $2)', [title, description]);
+        console.log(`Tâche "${title}" ajoutée avec succès mais elle n'est pas sauvegardé en base de donnée.`);
         resolve();
       });
     });
@@ -70,10 +85,18 @@ async function addTask() {
 async function changeStateTask() {
   return new Promise(resolve => {
     question("Entrez l'id de la tâche à changer", async (id) => {
-      question('Entrez le nouveau statut de la tâche ("en cours", c ou "terminee", t) :', async (status, retry) => {
-        status = status === 'c' ? 'en cours' : (status === 't' || status === 'terminee' ? 'terminée' : null);
-        if (!status) {
+      question('Entrez le nouveau statut de la tâche ("en cours", c ou "terminee", t) :', async (statut, retry) => {
+        statut = statut === 'c' ? 'en cours' : (statut === 't' || statut === 'terminee' ? 'terminée' : null);
+        if (!statut) {
           retry('Statut invalide, essayez à nouveau');
+          return;
+        }
+
+        const taskNotSaved = tasksNotSaved.find(task => task.id === parseInt(id) && task.statut !== 'terminée');
+        if (taskNotSaved) {
+          taskNotSaved.statut = statut;
+          console.log(`Statut de la tâche ${id} changé avec succès.`);
+          resolve();
           return;
         }
 
@@ -85,7 +108,7 @@ async function changeStateTask() {
           return;
         }
 
-        await client.query('UPDATE taches SET statut = $1 WHERE id = $2', [status, id]);
+        await client.query('UPDATE taches SET statut = $1 WHERE id = $2', [statut, id]);
         console.log(`Statut de la tâche ${id} changé avec succès.`);
         resolve();
       });
